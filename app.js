ec52fbe07dbd4a4445d523abd640bb73206782f1
Terence <terencegriff92@gmail.com>
2020-06-10 15:17:58 -0400

    added detection for the ball reaching the goal

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { Engine, Render, Runner, World, Bodies, Body } = Matter;
+const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 15;
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -144,13 +144,16 @@ verticals.forEach((row, rowIndex) => {
 
 // Goal
 const goal = Bodies.rectangle(width - unitLength / 2, height - unitLength / 2, unitLength * 0.7, unitLength * 0.7, {
+	label: 'goal',
 	isStatic: true
 });
 
 World.add(world, goal);
 
 // Ball
-const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4);
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+	label: 'ball'
+});
 World.add(world, ball);
 
 document.addEventListener('keydown', (event) => {
@@ -168,4 +171,15 @@ document.addEventListener('keydown', (event) => {
 	if (event.keyCode === 65) {
 		Body.setVelocity(ball, { x: x - 5, y });
 	}
+});
+
+// Win Condition
+Events.on(engine, 'collisionStart', (event) => {
+	event.pairs.forEach((collision) => {
+		const labels = [ 'ball', 'goal' ];
+
+		if (labels.includes(collision.bodyA.label) && labels.includes(collision.bodyB.label)) {
+			console.log('User won!');
+		}
+	});
 });
