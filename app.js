f126dbf98a84bdee41c37bffd92fb0de39442612
Terence <terencegriff92@gmail.com>
2020-06-09 23:17:35 -0400

    create the logic for maze generation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -26,3 +27,25 @@ const walls = [
 	Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
 ];
 World.add(world, walls);
+
+// Maze generation
+
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+	// If I have visited the cell at [row, column], then return
+	// Mark this cell as visited
+	// Assemble randomly-ordered list of neighbors
+	// For each neighbor...
+	// See if that neighbor is out of bounds
+	// If we have visited that neighbor, continue to next neighbor
+	// Remove a wall from either horizontals or verticals array
+	// Visit that next cell
+};
+
+stepThroughCell(startRow, startColumn);
